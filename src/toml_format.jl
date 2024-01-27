e23ec345482fe623e4c99cafd8eacd8e548e9484
Michael Goerz <mail@michaelgoerz.net>
2024-01-27 14:02:13 -0500

    Revise TOML format
    Instead of an `[Inventory]` section, the TOML format now has to start
    with a comment line that declares the format version. The header fields
    `project` and `version` are now top-level keys.
    
    This looks cleaner, saves a few bytes, and introduces unique "magic
    bytes" at the beginning of the file so that an `inventory.toml` file
    could be recognized as a distinct fromat by FileIO.jl.
    
    Also, `version` is now optional.
    
    Most previous (experimental) `inventory.toml` files can still be read
    without error.

--- src/toml_format.jl
+++ src/toml_format.jl
@@ -25,47 +25,50 @@ function Base.show(io::IO, ::MIME"application/toml", inventory::Inventory)
         push!(domains[item.domain][item.role], item_data)
     end
 
-    toml_dict = Dict(
-        "Inventory" => Dict(
-            "format" => "DocInventories v0",
-            "project" => inventory.project,
-            "version" => inventory.version,
-        ),
-        domains...
-    )
+    toml_dict = Dict("project" => inventory.project, domains...)
+    if !isempty(inventory.version)
+        toml_dict["version"] = inventory.version
+    end
 
+    TOML.println(io, "# DocInventory version 0")
     TOML.print(io, toml_dict; sorted=true)
 
 end
 
 
 function read_inventory(io::IO, ::MIME"application/toml")
+    format_header = readline(io)
+    if !startswith(format_header, "#")
+        @warn "Invalid format_header: $format_header"
+        # TODO: verify format_header more strictly once the format has settled.
+    end
     data = TOML.parse(io)
     try
-        inventory_format = data["Inventory"]["format"]
-        if inventory_format != "DocInventories v0"
-            msg = "Invalid inventory format: $(repr(inventory_format))"
-            throw(InventoryFormatError(msg))
-        end
-        project = data["Inventory"]["project"]
-        version = data["Inventory"]["version"]
+        project = string(pop!(data, "project"))   # mandatory
+        version = string(pop!(data, "version", ""))  # optional
         items = InventoryItem[]
         for (domain, domain_data) in data
-            (domain == "Inventory") && continue  # that's the header, not a domain
-            for (role, role_data) in domain_data
-                for item_data in role_data
-                    push!(
-                        items,
-                        InventoryItem(
-                            item_data["name"],
-                            domain,
-                            role,
-                            get(item_data, "priority", (domain == "std") ? -1 : 1),
-                            item_data["uri"],
-                            get(item_data, "dispname", "-")
+            if domain_data isa Dict
+                for (role, role_data) in domain_data
+                    for item_data in role_data
+                        push!(
+                            items,
+                            InventoryItem(
+                                item_data["name"],
+                                domain,
+                                role,
+                                get(item_data, "priority", (domain == "std") ? -1 : 1),
+                                item_data["uri"],
+                                get(item_data, "dispname", "-")
+                            )
                         )
-                    )
+                    end
                 end
+            elseif domain == "format"
+                # For backward-compatibility, remove this `elseif` in v1.0
+                @warn "Unexpected key: $domain"
+            else
+                throw(InventoryFormatError("Unexpected key: $domain"))
             end
         end
         return project, version, items
